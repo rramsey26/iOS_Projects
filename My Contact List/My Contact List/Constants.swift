aaf2e44dced963bf250ba7a75b1cadc114cd5d86
Ronald Ramsey <49212689+rramsey26@users.noreply.github.com>
2024-03-24 00:18:17 -0400

    Updated Settings UI

---
+++ My Contact List/My Contact List/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  My Contact List
+//
+//  Created by Ronald Ramsey on 3/23/24.
+//
+
+import Foundation
+struct Constants {
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
--- My Contact List/My Contact List/SettingsViewController.swift
+++ My Contact List/My Contact List/SettingsViewController.swift
@@ -7,15 +7,71 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pckSortField: UIPickerView!
+    @IBOutlet weak var swAscending: UISwitch!
+    
+    let sortOrderItems: Array<String> = ["ContactName", "City", "Birthday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        // Set the UI based on values in UserDefaults
+        let settings = UserDefaults.standard
+        swAscending.setOn(settings.bool(forKey: Constants.kSortDirectionAscending), animated: true)
+        let sortField = settings.string(forKey: Constants.kSortField)
+        var i = 0
+        for field in sortOrderItems {
+            if field == sortField {
+                pckSortField.selectRow(i, inComponent: 0, animated: false)
+            }
+            i += 1
+        }
+        pckSortField.reloadComponent(0)
+    }
+        
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        let settings = UserDefaults.standard
+        settings.set(swAscending.isOn, forKey: Constants.kSortDirectionAscending)
+        settings.synchronize()
+        print("Sort switch on: \(swAscending.isOn)")
+    }
+    
+    // MARK: - UIPickerViewdelegate Methods
+    
+    // Returns the number of 'columns' to display.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Returns the # of rows in the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+    
+    // Sets the value that is shown for each row in the picker
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    // If the user chooses from the pickerview, it calls this function
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let sortField = sortOrderItems[row]
+        let settings = UserDefaults.standard
+        settings.set(sortField, forKey: Constants.kSortField)
+        settings.synchronize()
+        print("Chosen item: \(sortOrderItems[row])")
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
