ab45592b49413a613a8edc9ea5ba6dd4f59a09dc
Ronald Ramsey <49212689+rramsey26@users.noreply.github.com>
2024-03-24 00:07:04 -0400

    Revert "Updated Settings and corrected project directory layout"
    This reverts commit 28eafa3ddc4b213a85ee9d7abb6b0aaf173253c9.

--- My Contact List/My Contact List/AppDelegate.swift
+++ My Contact List/My Contact List/AppDelegate.swift
@@ -14,17 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let settings = UserDefaults.standard
-        
-        if settings.string(forKey: Constants.kSortField) == nil {
-            settings.set("City", forKey: Constants.kSortField)
-        }
-        if settings.string(forKey: Constants.kSortDirectionAscending) == nil {
-            settings.set(true, forKey: Constants.kSortDirectionAscending)
-        }
-        settings.synchronize()
-        print("Sort field: \(settings.string(forKey: Constants.kSortField)!)")
-        print("Sort direction: \(settings.bool(forKey: Constants.kSortDirectionAscending))")
         return true
     }
 
